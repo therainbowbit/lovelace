381d5836801b0e64291befb5f03a1849ff42feb5
Alice <33951389+therainbowbit@users.noreply.github.com>
2017-11-24 01:45:07 -0600

    Uploaded source file
    First upload of unfinished lovelace.py

---
+++ lovelace.py
@@ -0,0 +1,46 @@
+from socket import * 
+import os, sys, struct
+
+
+def tcp_sniff(): 
+	
+	#Open up a raw socket, only TCP packets to be sniffed
+	try:
+		tcp_sock = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)
+
+	except Exception, error:
+
+		print "\n + Failed to create socket: %s \n" % error 
+		sys.exit()
+	
+	
+	
+	tcp_packet = tcp_sock.recvfrom(1024) 
+	
+	#Get the source IP from recvfrom's second tuple member
+	origin_addr = tcp_packet[1]
+	
+	#Ignore weird flag data and isolate only the IP address
+	origin_addr = origin_addr[0]
+	
+
+	#Get the packet data from recvfrom's first tuple member
+	packet_data = tcp_packet[0]
+
+	#Get IP header from first twenty characters
+	ip_header = packet_data[:20] 
+
+	#Now unpack the header data 
+	data = struct.unpack('!BBHHHBBH4s4s', ip_header)
+	
+	print data[0] >> 4 
+
+	 
+
+	   
+	
+	 
+
+
+while 1:
+	tcp_sniff() 
